98665ec6457c0497a9b6bfb9ca8f593feca8b89e
Michell Cronberg <michell@cronberg.dk>
2023-04-17 14:37:37 +0200

    Add project files.

---
+++ Variabler/Program.cs
@@ -0,0 +1,12 @@
+﻿{
+    var t = 1;
+    t++;
+    t += 10;
+    t -= 5;
+    Console.WriteLine(t);
+}
+
+{
+    double a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+    Console.WriteLine(a);
+}
